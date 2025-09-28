86ca182683300d91ca5820d5c7a5ac38eb30fc62
diixo <anatolii.podoba@gmail.com>
2025-09-28 10:33:18 +0300

    utils.py: small update

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 import re
 
-def remove_punctuation_wo_comma(text):
+def remove_punctuation_comma(text):
     cleaned_text = re.sub(r"[^\w\s!,]", "", text)
     return cleaned_text
 
 
-def parse_answer(text: str,eos_token):
+def parse_answer(text: str, eos_token):
     text = text.replace(eos_token,"")
     try:
         answer_start = text.index("Answer:")
-        answer = remove_punctuation_wo_comma(text[answer_start:].split(":")[1].strip().split("\n")[0])
+        answer = remove_punctuation_comma(text[answer_start:].split(":")[1].strip().split("\n")[0])
     except:
         answer = ""
     return answer
