475124211cb69a572850c13a249aaa21c827e11d
diixo <anatolii.podoba@gmail.com>
2025-10-06 02:25:24 +0300

    data.py

--- data.py
+++ data.py
@@ -37,6 +37,14 @@ paths = {
             "train": "datasets/qa25-shopping-interested-turns_train.txt",
             "test": "datasets/qa25-shopping-interested-turns_test.txt",
         },
+        "qa26": {
+            "train": "datasets/qa-dev_train.txt",
+            "test":  "datasets/qa-dev_test.txt",
+        },
+        "qa27": {   # turns
+            "train": "datasets/qa-dev-turns_train.txt",
+            "test":  "datasets/qa-dev-turns_test.txt",
+        },
     },
     "en-10k": {
         "qa22": {
